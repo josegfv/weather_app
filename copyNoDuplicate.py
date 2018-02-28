597d4743bc7d335f3e6931f370c52768ce25c57f
Jose Fernandez <josegfv@gmail.com>
2018-02-28 00:26:48 -0500

    Minor cleanup in file

--- copyNoDuplicate.py
+++ copyNoDuplicate.py
@@ -1,9 +1,11 @@
 import csv
 
+
 def main():
     singleValues = []
 
-    fdst = open("./GeoLiteCity-Location-short.csv", "w", encoding='utf-8', errors='ignore')
+    fdst = open("./GeoLiteCity-Location-short.csv",
+                "w", encoding='utf-8', errors='ignore')
     writer = csv.writer(fdst)
 
     with open('./GeoLiteCity-Location.csv', 'r', encoding='utf-8', errors='replace') as fsrc:
@@ -14,10 +16,9 @@ def main():
                 singleValues.append(lc)
                 writer.writerow(row)
                 #print("Line added for city: {}".format(lc.encode('utf-8')))
-                
+
     fsrc.close()
     fdst.close()
-
 
 if __name__ == '__main__':
     main()
